68c14052329ce34e31bd55067b9046ca345a7d7d
ebarbosa27 <ebarbosa5449@gmail.com>
2025-06-11 20:56:34 -0500

    Created JS file with starter code for the html calls and event type

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * @typedef Event
+ * @property {number} id
+ * @property {string} name
+ * @property {string} description
+ * @property {string} date
+ * @property {string} location
+ * @property {number} cohortId
+ */
+
+// HTML setup
+const BASE_URL = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
+const COHORT_CODE = "/2504-FTB-ET-WEB-PT";
+const APP = "/events";
+const api = BASE_URL + COHORT_CODE + APP;
+
+// state variables
